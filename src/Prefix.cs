f3689f32c198b4ff5e5156cf9fd1e1e28d2ae513
Nas <46555162+NascuBB@users.noreply.github.com>
2021-09-20 20:13:38 +0300

    v1
    First version of custom prefixes for Discord bot

---
+++ src/Prefix.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Discord;
+using System.IO;
+using ShablePrefics.ServerIds;
+
+namespace ShablePrefics
+{
+    public static class Prefix
+    {
+        //!
+        private static string ConfigFolder = "Resourses";
+        private static string ConfigFile = "Prefixes.json";
+        private static string ConfigPath = ConfigFolder + "/" + ConfigFile;
+
+       /// <summary>
+       /// Sets prefix to a chosen guild
+       /// </summary>
+        public static Task SetPrefix(IGuild guild, string prefix)
+        {
+            string text = "";
+            ulong id = guild.Id;
+            try
+            {
+                //checking for directory
+                if (!Directory.Exists(ConfigFolder))
+                {
+                    Directory.CreateDirectory(ConfigFolder);
+                }
+                //checking if file with prefixes exists
+                if (!File.Exists(ConfigPath))
+                {
+                    //if no, creating a new one and writing first guild with its prefix
+                    string[] str = { "test", $"{id}={prefix}" };
+                    var rem = new Shable
+                    {
+                        PrefToId = str
+                    };
+                    text = JsonConvert.SerializeObject(rem);
+                    File.WriteAllText(ConfigPath, text);
+                }
+                else
+                {
+                    bool add = false;
+                    var item = id.ToString();
+                    //deserializing json file with prefixes
+                    var obj = JsonConvert.DeserializeObject<Shable>(File.ReadAllText(ConfigPath));
+                    //getting list of guilds
+                    string[] ids = obj.PrefToId;
+                    //searching for requested guild
+                    foreach(string str in ids)
+                    {
+                        if (str.Contains(item))
+                        {
+                            add = true;
+                            break;
+                        }
+
+                    }
+                    if (add)
+                    {
+                        
+                        //finding index of guild in list
+                        int index = Array.FindIndex(ids,x => x.Contains(item));
+                        //copying item from list
+                        string desiredStr = ids[index];
+                        //splitting string on two halfs, one is guild id and second is its prefix
+                        string[] half = desiredStr.Split('=');
+                        //overwriting prefix
+                        string strin = $"{half[0]}={prefix}";
+                        //overwriting item
+                        ids[index] = strin;
+                        //serealizing and rewriting json document
+                        var rem = new Shable
+                        {
+                            PrefToId = ids
+                        };
+                        text = JsonConvert.SerializeObject(rem);
+                        File.WriteAllText(ConfigPath, text);
+                    }
+                    else
+                    {
+                        string newId = $"{id}={prefix}";
+                        string[] result = Ext.Append(ids, newId);
+                        var rem = new Shable
+                        {
+                            PrefToId = result
+                        };
+                        text = JsonConvert.SerializeObject(rem);
+                        File.WriteAllText(ConfigPath, text);
+                    }
+                }
+
+                return Task.CompletedTask;
+            }
+            catch (Exception ex)
+            {
+                return Task.FromException(ex);
+            }
+        }
+        /// <summary>
+        /// Gets prefix to a requered guild
+        /// </summary>
+        /// <returns>Prefix of requested guild</returns>
+        public static Task<string> GetPrefix(IGuild guild, string defaultPrefix)
+        {
+            try
+            {
+                //checking for directory
+                if (!Directory.Exists(ConfigFolder))
+                {
+                    Directory.CreateDirectory(ConfigFolder);
+                }
+                //checking if file with prefixes exists
+                if (!File.Exists(ConfigPath))
+                {
+                    throw new Exception("client doesn't have special file. Try set prefix once");
+                }
+                bool add = false;
+                ulong id = guild.Id;
+                string item = id.ToString();
+                //deserializing json file with prefixes
+                var obj = JsonConvert.DeserializeObject<Shable>(File.ReadAllText(ConfigPath));
+                //getting list of guilds
+                string[] ids = obj.PrefToId;
+                foreach (string str in ids)
+                {
+                    if (str.Contains(item))
+                    {
+                        add = true;
+                        break;
+                    }
+                    
+                }
+                if (add)
+                {
+                    //finding index of requested guild
+                    int index = Array.FindIndex(ids, x => x.Contains(item));
+                    string desiredStr = ids[index];
+                    //splitting string
+                    string[] halfs = desiredStr.Split('=');
+                    //getting prefix
+                    string prefix = halfs[1];
+
+                    return Task.FromResult(prefix);
+                }
+                else
+                {
+                    return Task.FromResult(defaultPrefix);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        /// <summary>
+        /// removes custom prefix for chosen guild
+        /// </summary>
+        public static Task RemovePrefix(IGuild guild)
+        {
+
+            try
+            {
+
+                bool add = false;
+                ulong id = guild.Id;
+                string item = id.ToString();
+                //deserializing json file with prefixes
+                var obj = JsonConvert.DeserializeObject<Shable>(File.ReadAllText(ConfigPath));
+                //getting list of guilds
+                string[] ids = obj.PrefToId;
+                foreach (string str in ids)
+                {
+                    if (str.Contains(item))
+                    {
+                        //finding index with guild we want to delete
+                        int RemIndex = Array.FindIndex(ids, x => x.Contains(item));
+                        //removing guild and its prefix
+                        ids = ids.Where((source, index) => index != RemIndex).ToArray();
+                        //overwriting document
+                        var rem = new Shable
+                        {
+                            PrefToId = ids
+                        };
+                        var text = JsonConvert.SerializeObject(rem);
+                        File.WriteAllText(ConfigPath, text);
+                        return Task.CompletedTask;
+                    }
+                }
+                //very buggy
+                if (add == true)
+                {
+                    throw new Exception("This server has no custom prefix");
+                }
+                else
+                {
+                    return Task.CompletedTask;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                return Task.FromException(ex);
+            }
+        }
+    }
+}
